ffe2da58f741db125119dca784f571ae72e69328
aliciaguerra <aiguerra@Alicias-MacBook-Pro.local>
2015-03-22 13:22:37 -0700

    capp2

---
+++ main.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(void){
+    int x0,x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,x12,x13,x14,x15;
+    int y0,y1,y2,y3,y4,y5,y6,y7,y8,y9,y10,y11,y12,y13,y14,y15;
+    int crossover;
+printf("Enter Genotype 1:");
+scanf("%d%d%d%d",&x0,&x1,&x2,&x3);
+printf("%d %d %d %d", x0,x1,x2,x3);
+}
